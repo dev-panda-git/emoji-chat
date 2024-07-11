8c81fff0e90cd5ff587e060ba665144886bc5629
dev_panda <tohirbabs@gmail.com>
2024-07-11 17:58:10 +0100

    app updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,17 @@
 import { addDoc, collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { db } from "./firebase";
+import { EmojiMessage } from "./components/EmojiMessage";
+import { EmojiForm } from "./components/EmojiForm";
 
 function App() {
   const user = {
     id: "04",
-    dp: "⚽",
+    dp: "🎲",
   };
 
   const [inputValue, setInputValue] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [isPosting, setIsPosting] = useState(false);
   const [messages, setMessages] = useState([]);
-
-  const emojiRegex =
-    /(?:[\u2700-\u27bf] | [\uFE00-\uFE0F] | [\u1f300-\u1f5ff] | [\u1f600-\u1f64f] | [\u1f680-\u1f6ff] | [\u2600-\u26ff] | [\u2700-\u27bf] | [\u1f1e6-\u1f1ff] | [\u1f900-\u1f9ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1ff] | [\u1f1e6-\u1f1f])+/g;
-
-  const handleInputChange = (event: any) => {
-    const newInputValue = event.target.value;
-    setInputValue(newInputValue);
-
-    if (emojiRegex.test(newInputValue)) {
-      setErrorMessage("");
-    } else {
-      setErrorMessage("Please enter only emojis.");
-    }
-  };
 
   const fetchMsgs = async () => {
     await getDocs(collection(db, "messages")).then((querySnapshot) => {
@@ -42,86 +28,42 @@ function App() {
     fetchMsgs();
   }, []);
 
-  const handleSubmit = async (e: any) => {
-    console.log(inputValue);
+  const sortedMessages = messages.sort((messageA, messageB) => {
+    return new Date(messageA.createdAt) - new Date(messageB.createdAt);
+  });
 
-    // if (!emojiRegex.test(inputValue)) {
-    //   setErrorMessage("Please enter only emojis.");
-    //   return;
-    // }
-
-    setIsPosting(true); // Set Posting state to true
-
-    e.preventDefault();
-
-    try {
-      const docRef = await addDoc(collection(db, "messages"), {
-        text: inputValue,
-        author: "⚽",
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      }); // Add emoji to Firestore
-      console.log("Document written with ID: ", docRef.id);
-
-      fetchMsgs();
-      setInputValue(""); // Clear input after successful submission
-    } catch (error) {
-      console.error("Error adding emoji:", error);
-      setErrorMessage("An error occurred. Please try again.");
-    }
-  };
-
-  const Msg = ({ emoji, author }: any) => {
-    return author === user.dp ? (
-      <div className="relative h-28 flex items-center">
-        <div className="flex gap-2 items-end w-max absolute right-0">
-          <div className="p-5 rounded-t-[2rem] rounded-bl-2xl rounded border border-white/10 bg-blue-300/30 tracking-widest text-3xl w-max">
-            {emoji}
-          </div>
-          <div className="text-2xl">{author}</div>
-        </div>
-      </div>
-    ) : (
-      <div className="relative h-28 flex items-center">
-        <div className="flex gap-2 items-end w-max">
-          <div className="text-2xl">{author}</div>
-          <div className="p-5 rounded-t-[2rem] rounded-br-2xl rounded border border-white/10 bg-blue-300/30 tracking-widest text-3xl w-max">
-            {emoji}
-          </div>
-        </div>
-      </div>
-    );
-  };
+  console.log(sortedMessages);
 
   return (
-    <div className="bg-black  relative ">
+    <div className="bg-black  relative h-screen w-screen flex items-center justify-center">
+      {/* <div className="bg-white/40 backdrop-blur-md rounded-3xl m-auto max-w-xl h-[50vh] w-[98%] overflow-y-scroll absolute z-10">
+        <p className="p-3 text-4xl ">pick a usermoji</p>
+        <div className="flex text-5 xl items-center justify-center p-2 gap-5 flex-wrap">
+          <div className="p-5 rounded-3xl bg-green-200">🐼</div>
+          <div className="p-5 rounded-3xl bg-green-200">🥶</div>
+          <div className="p-5 rounded-3xl bg-green-200">🥶</div>
+          <div className="p-5 rounded-3xl bg-green-200">🐼</div>
+          <div className="p-5 rounded-3xl bg-green-200">🐼</div>
+          <div className="p-5 rounded-3xl bg-green-200">🥶</div>
+          <div className="p-5 rounded-3xl bg-green-200">🥶</div>
+        </div>
+      </div> */}
       <div className="w-full h-screen overflow-y-scroll max-w-2xl mx-auto relative ">
         <div className="p-2 py-5 bg-black/30 backdrop-blur-md sticky top-0 z-10">
           <h1 className="font-bold text-4xl text-center">emoji chat</h1>
-          <span>{errorMessage}</span>
-          <span>{isPosting ? "posting" : "not posting"}</span>
         </div>
         <div className="relative w-full p-6 md:px-10">
-          {messages.map((msg) => (
-            <Msg key={msg.id} emoji={msg.text} author={msg.author} />
+          {sortedMessages.map((msg) => (
+            <EmojiMessage
+              key={msg.id}
+              emoji={msg.text}
+              author={msg.author}
+              time={msg.createdAt}
+              user={user}
+            />
           ))}
         </div>
-        <div className="sticky  p-4 md:px-10 w-full bottom-0 z-10">
-          <div className="flex relative  items-center  w-full text-2xl">
-            <input
-              type="text"
-              value={inputValue}
-              onChange={handleInputChange}
-              placeholder="Enter emoji"
-              className=" w-full bg-white/30 p-5 pl-8 pr-24  rounded-full backdrop-blur-md"
-            />
-            <button
-              onClick={handleSubmit}
-              className="absolute right-0 p-5 px-8 rounded-full bg-white text-black"
-            >
-              post
-            </button>
-          </div>
-        </div>
+        <EmojiForm fetchMsgs={fetchMsgs} />
       </div>
     </div>
   );
